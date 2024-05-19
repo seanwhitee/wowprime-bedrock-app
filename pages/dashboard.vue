7eaac290878d8a0562e78f81d7e789f1a434a65c
Sean <91260510+seanwhitee@users.noreply.github.com>
2024-05-19 11:06:50 +0800

    Set images to w-full.

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -91,7 +91,7 @@ const mainTitle = "品味分析";
       <UCarousel v-slot="{ item }" :items="items">
         <div class="w-[70vw] h-[300px] object-cover">
           <img :src="`https://wangpin-web-bucket.s3.us-west-2.amazonaws.com/logo/${item}.png`" class="absolute p-1 w-16 h-16 aspect-squares">
-          <img :src="`https://wangpin-web-bucket.s3.us-west-2.amazonaws.com/${item}.jpg`" draggable="false" />
+          <img :src="`https://wangpin-web-bucket.s3.us-west-2.amazonaws.com/${item}.jpg`" draggable="false" class="w-full" />
         </div> 
       </UCarousel>
     </div>
